9088d2c0afd6d01b7e7c1d90147e909b43fa1885
sky <skybride@hotmail.com>
2021-02-10 19:22:07 -0500

    add status

--- scripts.js
+++ scripts.js
@@ -3,12 +3,27 @@ let startBtn = document.createElement("button");
 let exitBtn = document.createElement("button");
 let inputField = document.createElement("input");
 let enterBtn = document.createElement("button");
+let img = document.createElement("img");
+let inputHealth = document.createElement("input");
+let statsText = document.createElement("p");
+let statsTotalText = document.createElement("p");
+let addBtn = document.createElement("button");
+
+let statsTotal = 30;
 
 let userName = "";
 
 startBtn.textContent = "Start";
 exitBtn.textContent = "Exit";
 enterBtn.textContent = "Enter";
+statsText.innerHTML = "<b>Health</b>";
+statsTotalText.innerHTML = `<b>Total Stats: ${statsTotal}</b>`;
+inputHealth.value = 1;
+addBtn.textContent = "+";
+
+// img.src = "images/home.png";
+// img.setAttribute("src", "images/home.png");
+img.setAttribute("src", "images/character.png");
 
 menu.append(startBtn);
 menu.append(exitBtn);
@@ -17,6 +32,8 @@ menu.append(exitBtn);
 startBtn.classList.add("start");
 exitBtn.classList.add("end");
 enterBtn.classList.add("enter");
+inputHealth.classList.add("health");
+addBtn.classList.add("add");
 
 //Functions
 
@@ -27,6 +44,32 @@ const grabUserName = userName => {
 
 const greeting = userName => {
     menu.textContent = "Welcome " + userName;
+    // menu.append(img);
+    status();
+}
+
+const status = () => {
+    
+
+    menu.append(img);
+    menu.append(statsText);
+    menu.append(inputHealth);
+    menu.append(addBtn);
+    menu.append(statsTotalText);
+    
+}
+
+const addStat = () => {
+    
+    let intHealth = parseInt(inputHealth.value);
+
+    if(intHealth < 10) {
+        intHealth++;
+        statsTotal--;
+
+        inputHealth.value = intHealth;
+        statsTotalText.innerHTML = `<b>Total Stats: ${statsTotal}</b>`;
+    }
 }
 
 const startGame = () => {
@@ -42,6 +85,8 @@ const endGame = () => {
     menu.textContent = "GOODBYE!";
 }
 
+
 //Adding Events
 startBtn.addEventListener("click", startGame);
 exitBtn.addEventListener("click", endGame);
+addBtn.addEventListener("click", addStat);
